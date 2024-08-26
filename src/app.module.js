d8f54f4af012589499864aad76e2d0d23d0b8baf
Aritra Bera <aritrabera69@gmail.com>
2024-08-27 00:50:05 +0530

    project setup

---
+++ src/app.module.js
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ContactsModule } from './contacts/contacts.module';
+
