c16103e9dbcdcecc41204b640b5adb8811048030
Aritra Bera <aritrabera69@gmail.com>
2024-08-27 00:55:24 +0530

    project setup

--- src/app.module.js
+++ src/app.module.js
@@ -4,3 +4,21 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ContactsModule } from './contacts/contacts.module';
 
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'your_postgres_user',
+      password: 'your_postgres_password',
+      database: 'your_database_name',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule,
+    ContactsModule,
+  ],
+})
+export class AppModule {}
